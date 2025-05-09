6eeeb9515b9bf7938dfd1ae100f8eecab32281bc
JhonnyRocha796 <202000670@est.umss.edu>
2025-05-09 16:27:34 -0400

    correccion bug 612
    boton  de Cerrar cambiado para que sea Ver mas y aun no existe otro apartado donde nos pueda redirigir el boton de ver mas

--- my-app/src/app/Notificaciones/componentes/componentsModales/ModalDetallesRenta.tsx
+++ my-app/src/app/Notificaciones/componentes/componentsModales/ModalDetallesRenta.tsx
@@ -78,7 +78,7 @@ const ModalDetallesRenta = ({ isOpen, notification, onClose, onDelete }: ModalPr
             onClick={onClose}
             className="cursor-pointer bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600"
           >
-            Cerrar
+            Ver mas
           </button>
         </div>
       </div>
