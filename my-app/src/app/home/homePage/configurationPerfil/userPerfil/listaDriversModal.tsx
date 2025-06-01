3c6f9a56bd0f2f2940d69b4247718f2fd441afc5
Melina Montaño <202200060@est.umss.edu>
2025-06-01 12:05:10 -0400

    Corrección del modal

--- my-app/src/app/home/homePage/configurationPerfil/userPerfil/listaDriversModal.tsx
+++ my-app/src/app/home/homePage/configurationPerfil/userPerfil/listaDriversModal.tsx
@@ -21,7 +21,7 @@ const DriversModal: React.FC<Props> = ({ isOpen, onClose }) => {
 
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center"
+      className="fixed inset-0 bg-opacity-50 z-50 flex items-center justify-center"
       onClick={onClose}
     >
       <div
