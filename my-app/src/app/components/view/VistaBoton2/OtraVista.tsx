11c40a1b37d213c53c99c8016632b4b45f4e63a5
LuisHerlan7 <luis321herlan@gmail.com>
2025-06-10 22:40:57 -0400

    carpetas mapas

--- my-app/src/app/components/view/VistaBoton2/OtraVista.tsx
+++ my-app/src/app/components/view/VistaBoton2/OtraVista.tsx
@@ -1,8 +1,10 @@
+import MapaConFiltrosEstaticos from "@/app/components/mapa/mapas";
 export default function OtraVista() {
   return (
     <div className="text-2xl text-center text-[var(--azul-oscuro)] font-bold bg-amber-500 h-160 flex justify-center w-full">
       <div className="bg-amber-300 w-full">
         hola
+        <MapaConFiltrosEstaticos />
       </div>
       <div className="bg-amber-400 w-full">
         hola
